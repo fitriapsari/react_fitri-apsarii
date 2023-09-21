6763ea01a3fd0291327c4bb70f6b82dcbdd619c6
fitriapsari <fitriapsarii@students.unnes.ac.id>
2023-09-21 16:17:07 +0700

    update:add summary

---
+++ 16_React Routing/praktikum/landingPage/src/componen/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+function Navbar() {
+  return (
+    <header className="nav-menu">
+      <h6>Simple Header</h6>
+      <ul className="nav-menu">
+        <li>
+          <a href="/home">Home</a>
+        </li>
+        <li>
+          <a href="/fetures" className="nav-menu">
+            Features
+          </a>
+        </li>
+        <li>
+          <a href="/pricing" className="nav-menu">
+            Pricing
+          </a>
+        </li>
+        <li>
+          <a href="/FAQs" className="nav-menu">
+            FAQs
+          </a>
+        </li>
+        <li>
+          <a href="/about" className="nav-menu">
+            About
+          </a>
+        </li>
+      </ul>
+      <hr />
+    </header>
+  )
+}
+export default Navbar
