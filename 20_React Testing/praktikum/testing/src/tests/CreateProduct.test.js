ddcc4a45664fc45e7e9c501a5b745f50f059ecc2
fitriapsari <fitriapsarii@students.unnes.ac.id>
2023-10-11 16:37:54 +0700

    update summary dan testing

--- 20_React Testing/praktikum/testing/src/tests/CreateProduct.test.js
+++ 20_React Testing/praktikum/testing/src/tests/CreateProduct.test.js
@@ -1,67 +1,77 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import CreateProduct from "./CreateProduct"
+import CreateProduct from "./CreateProduct";
 
-describe('CreateProduct', () => {
-  it('renders headline', () => {
+describe("CreateProduct", () => {
+  it("renders headline", () => {
     render(<CreateProduct />);
-    // const headlines = screen.getAllByText(/Product Name/i);
-    // expect(headlines[0]).toBeInTheDocument();
-    expect(screen.getByText('Product Name: ')).toBeInTheDocument()
-    expect(screen.getByText('Product Category: ')).toBeInTheDocument()
-    expect(screen.getByText('Image Product: ')).toBeInTheDocument()
-
+    expect(screen.getByText("Product Name: ")).toBeInTheDocument();
+    expect(screen.getByText("Product Category: ")).toBeInTheDocument();
+    expect(screen.getByText("Image Product: ")).toBeInTheDocument();
   });
 
-  it('render all input forms', () => {
+  it("render all input forms", () => {
     render(<CreateProduct />);
 
-    fireEvent.change.(screen.getByLabelText('Product Name: '), {
-      target: { value: 'Product 1' },
+    const inputProductName = screen.getByLabelText("Product Name:");
+    fireEvent.change(inputProductName, {
+      target: { value: "Product 1" },
     });
-    fireEvent.change.(screen.getByLabelText('Product Category: '), {
-      target: { value: 'Baju' },
+
+    const inputProductCategory = screen.getByLabelText("Product Category:");
+    fireEvent.change(inputProductCategory, {
+      target: { value: "Baju" },
     });
-    fireEvent.change.(screen.getByLabelText('Product Price: '), {
-      target: { value: '50' },
+
+    const inputProductPrice = screen.getByLabelText("Product Price:");
+    fireEvent.change(inputProductPrice, {
+      target: { value: "50" },
     });
-    const imagefile = new File(['image contest'], 'image.png', {
+
+    const imageFile = new File(['image content'], 'image.png', {
       type: 'image/png',
     });
-    fireEvent.change.(screen.getByLabelText('Image of Product: '), {
-      target: { files: 'imageFile' },
+
+    const inputProductImage = screen.getByLabelText("Image of Product:");
+    fireEvent.change(inputProductImage, {
+      target: { files: [imageFile] },
     });
 
-    // submit 
-    fireEvent.change.(screen.getByLabelText('Product Name: '));
+    // submit
+    const submitButton = screen.getByText("Submit");
+    fireEvent.click(submitButton);
 
     // assert form submission
-    expect(screen.getByText('Product 1')).toBeInTheDocument()
-    expect(screen.getByText('Baju')).toBeInTheDocument()
-    expect(screen.getByText('$50')).toBeInTheDocument()
-    expect(screen.getByText('List Product')).toBeInTheDocument()
+    expect(screen.getByText("Product 1")).toBeInTheDocument();
+    expect(screen.getByText("Baju")).toBeInTheDocument();
+    expect(screen.getByText("$50")).toBeInTheDocument();
+    expect(screen.getByText("List Products")).toBeInTheDocument();
+  });
 
-    it('test product name input'.[] => {
-      render( <CreateProduct /> );
+  it('test product name input', () => {
+    render(<CreateProduct />);
+    const inputProductName = screen.getByLabelText('Product Name:');
+    fireEvent.change(inputProductName, {
+      target: { value: 'Baju' },
+    });
+    expect(screen.getByText('Baju')).toBeInTheDocument();
+  });
 
-    // selection terhadap element input 
-    const inputProductName = screen.getByLabelText('Product Name:')
-    // panggil change method untuk even headler change
-    // ambil value dari tiap event headler => e.target.value
-    fireEvent.change.apply(inputProductName, {
-      taget: { value: 'Baju' }
-    })
-    expect(screen.getByText('Baju')).toBeInTheDocument()
-  })
+  it('test product category input', () => {
+    render(<CreateProduct />);
+    const inputProductCategory = screen.getByLabelText('Product Category:');
+    fireEvent.change(inputProductCategory, {
+      target: { value: 'Celana' },
+    });
+    expect(screen.getByText('Celana')).toBeInTheDocument();
+  });
 
-  const inputProductName = screen.getByLabelText('Product Category:')
-  // panggil change method untuk even headler change
-  // ambil value dari tiap event headler => e.target.value
-  fireEvent.change.apply(inputProductCategory, {
-    taget: { value: 'Baju' }
-  })
-  expect(screen.getByText('Baju')).toBeInTheDocument()
-})
-
-
-})
+  it('test product price input', () => {
+    render(<CreateProduct />);
+    const inputProductPrice = screen.getByLabelText('Product Price:');
+    fireEvent.change(inputProductPrice, {
+      target: { value: '75' },
+    });
+    expect(screen.getByText('$75')).toBeInTheDocument();
+  });
+});
--- 27_Instalation Open AI in React/README.md
+++ 27_Instalation Open AI in React/README.md
@@ -1,3 +1,115 @@
 # Resume Materi Instalation Open AI
 
-###
+### Kenapa Belajar OpenAI
+
+1. Gratis
+2. Mudah dipasang dan mudah digunakan
+3. Dipakai banyak user
+4. Jumlah parameter: 175 miliar (model davici 03)
+
+### Starting Point
+
+- Setiap code dalam ppt sudah di upload dalam link: https://github.com/attoyibi/open-api/tree/main/basic-installation untuk memudahkan belajar
+
+- Dokumentasi OpenAI: https://platform.openai.com/docs/api-reference dan https://platform.openai.com/docs/introduction
+
+### Persiapan
+
+1. Pemahaman mengenai react
+2. Dapat menggunakan git/github atau sejenisnya
+3. Menginstall npm/yarn/etc
+4. Mempunyai code editor
+5. Laptop dan internet
+
+### Pemasangan OpenAI di React
+
+1. Membuat react project
+   <br/>
+
+   ```yarn create vite'''
+
+   ```
+
+2. Install package OpenAI
+   <br/>
+
+   ```npm install openai@^4.0.0'''
+
+   ```
+
+3. Import module yang dperlukan
+   <br/>
+   ```import { useState } from "react";
+   import { Configuration, OpenAIApi } from "openai"; '''
+   ```
+4. Deklarassi Object
+   <br/>
+
+```const configuration = new Configuration({
+    apiKey: "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",
+  });
+  const openai = new OpenAIApi(configuration);
+```
+
+5. Deklarasi State
+
+```const configuration = new Configuration({
+    apiKey: "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",
+  });
+  const openai = new OpenAIApi(configuration);
+```
+
+6. Deklarasi handleclick
+
+```const handleClick = async () => {
+    setLoading(true);
+    try {
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        // model: "gpt-3.5-turbo",
+        prompt: prompt,
+        temperature: 0.5,
+        max_tokens: 100,
+      });
+      setResult(response.data.choices[0].text);
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+  };
+```
+
+7. Rendering Komponen
+
+```
+ <main className="main">
+      <div className="w-2/4 mx-auto">
+        <textarea
+          type="text"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          placeholder="Write your prompt.."
+          className="textarea"
+        ></textarea>
+
+        <button
+          onClick={handleClick}
+          disabled={loading || prompt.length === 0}
+          className="btn"
+        >
+          {loading ? "Generating..." : "Generate"}
+        </button>
+
+        <pre className="result">{result}</pre>
+      </div>
+    </main>
+```
+
+### Cara Mendapatkan API Keys
+
+1. Buka situs web OpenAI => https://platform.openai.com/docs/api-reference
+2. Buatlah akun pada OpenAI
+3. Cari bagian introduction terdapat dalam tulisan API Keys warna biru lalu diklik
+4. Pilih letak API Keys dan create new secret key
+5. Ikuti petunjuk untuk mengkonfigurasi kunci API sesuai dengan kebutuhan
+6. Masukan API Keys ke dalam project React yang telah dibuat
